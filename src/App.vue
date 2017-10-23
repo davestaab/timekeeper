861a56a1b58b68af4dcde0a688866bf16feb8cee
Dave Staab <davestaab@gmail.com>
2017-10-23 12:42:32 -0400

    adding Timeline component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <Timeline></Timeline>
   </div>
 </template>
 
 <script>
+import Timeline from './components/Timeline';
 
 export default {
   name: 'app',
   components: {
+    Timeline,
   },
 };
 </script>
@@ -19,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Time Line</h1>
+    </div>
+</template>
+
+<script>
+import { getData } from '../utils';
+
+export default {
+  name: 'Timeline',
+  data() {
+    return {
+      data: getData(),
+    };
+  },
+};
+
+</script>
+
+<style scoped>
+
+</style>
