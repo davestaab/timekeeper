52cda3d4ff05c96a63e5ad79344ca3a0a2faad77
Dave Staab <dave.staab@cadre5.com>
2018-01-13 21:39:58 -0500

    clean up the chart some

--- src/components/TimeLineChart.vue
+++ src/components/TimeLineChart.vue
@@ -7,7 +7,6 @@ import TimeLineChart from '@/timeline/TimeLineChart'
 import { select } from 'd3'
 
 const chart = TimeLineChart()
-// .categories(['personal', 'scrum', 'dev', 'planning', 'testing'])
 
 export default {
   name: 'TimelineChart',
@@ -15,17 +14,14 @@ export default {
     'chartData',
   ],
   mounted: function () {
-    console.log('mounted!')
-    console.log('this', this)
     const entry = this.chartData[0]
-    console.log('chart data is: ', entry.data.map(inflate))
     chart
       .categories(entry.categories)
       .data(entry.data.map(inflate))
     select(this.$el).call(chart)
-    console.log('chart debug:', chart.debug())
   },
 }
+
 /**
  * Inflate the date object from a string (as stored in local storage) to a date object
  * @param  {[type]} d [description]
