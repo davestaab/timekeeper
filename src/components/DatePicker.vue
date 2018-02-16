d53b85f29b382aa1844d06726aa9d73f241a5d90
Dave Staab <staabdr@ornl.gov>
2018-02-15 20:54:20 -0500

    formatting with new eslint settings

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -7,44 +7,44 @@
 </template>
 
 <script>
-import moment from 'moment'
+  import moment from "moment";
 
-export default {
-  name: 'DatePicker',
-  props: {
-    currentDate: {
-      type: Date,
-      required: true,
+  export default {
+    name: "DatePicker",
+    props: {
+      currentDate: {
+        type: Date,
+        required: true
+      }
     },
-  },
-  computed: {
-    formattedDate: function () {
-      return moment(this.currentDate).format('MMM DD, YYYY')
+    computed: {
+      formattedDate: function() {
+        return moment(this.currentDate).format("MMM DD, YYYY");
+      }
     },
-  },
-  methods: {
-    nextDate: function (amount) {
-      // console.log('amount is: ', amount)
-      this.$emit('nextDate', amount)
-      // const next = this.current + amount
-      // this.current = next < 0 ? 0 : next >= this.data.length ? this.data.length - 1 : next
-    },
-  },
-}
+    methods: {
+      nextDate: function(amount) {
+        // console.log('amount is: ', amount)
+        this.$emit("nextDate", amount);
+        // const next = this.current + amount
+        // this.current = next < 0 ? 0 : next >= this.data.length ? this.data.length - 1 : next
+      }
+    }
+  };
 </script>
 
 <style scoped>
-.date-picker {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.date {
-  margin: 10px;
-  flex: 0 0 auto;
-}
-.arrow {
-  flex: 0 0 auto;
-  cursor: pointer;
-}
+  .date-picker {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .date {
+    margin: 10px;
+    flex: 0 0 auto;
+  }
+  .arrow {
+    flex: 0 0 auto;
+    cursor: pointer;
+  }
 </style>
