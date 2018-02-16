694c0d0f952af02d8fc093ace306747e2a86bf1f
Dave Staab <staabdr@ornl.gov>
2018-02-15 22:03:18 -0500

    adding categories

---
+++ src/components/Categories.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cat-container">
+    <h1>Categories</h1>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{cat}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'categories'
+    ]
+  };
+</script>
+
+
+<style scoped>
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    list-style: none;
+  }
+  .cat-container {
+    width: 760px;
+    margin: 0 auto;
+  }
+</style>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,44 +3,51 @@
     <h1>Time Line</h1>
     <date-picker :current-date="currentDate" @nextDate="nextDate"></date-picker>
     <time-line-chart :chart-data="currentData"></time-line-chart>
+    <div class="">
+      <categories :categories="currentData.categories"></categories>
+    </div>
   </div>
 </template>
 
 <script>
-import { getData } from '../utils'
-import moment from 'moment'
-import DatePicker from './DatePicker'
+  import { getData } from "../utils";
+  import moment from "moment";
+  import DatePicker from "./DatePicker";
+  import Categories from "./Categories";
 
-const TimeLineChart = () => import('./TimeLineChart')
+  const TimeLineChart = () => import("./TimeLineChart");
 
-export default {
-  name: 'Timeline',
-  data () {
-    return {
-      data: getData(),
-      current: 0,
+  export default {
+    name: "Timeline",
+    data() {
+      return {
+        data: getData(),
+        current: 0
+      };
+    },
+    computed: {
+      currentData: function() {
+        return this.data[this.current];
+      },
+      currentDate: function() {
+        return moment(this.currentData.date).toDate();
+      }
+    },
+    methods: {
+      nextDate: function(amount) {
+        const next = this.current + amount;
+        this.current =
+          next < 0 ? this.data.length - 1 : next >= this.data.length ? 0 : next;
+      }
+    },
+    components: {
+      TimeLineChart,
+      DatePicker,
+      Categories
     }
-  },
-  computed: {
-    currentData: function () {
-      return this.data[this.current]
-    },
-    currentDate: function () {
-      return moment(this.currentData.date).toDate()
-    },
-  },
-  methods: {
-    nextDate: function (amount) {
-      const next = this.current + amount
-      this.current = next < 0 ? this.data.length - 1 : next >= this.data.length ? 0 : next
-    },
-  },
-  components: {
-    TimeLineChart,
-    DatePicker,
-  },
-}
+  };
 </script>
 
 <style scoped>
+
 </style>
