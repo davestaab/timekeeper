32545dedccf230450a602d6ac6807c783de1f469
Dave Staab <dave.staab@cadre5.com>
2018-08-17 08:50:30 -0400

    pumped up eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
   },
   extends: ['plugin:vue/recommended', '@vue/prettier'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/require-default-prop': 'error',
+    'vue/require-prop-types': 'error',
+    'vue/order-in-components': 'error'
   }
 };
