c874e549e7f88633e8ffc67a826f5a403bcae6c0
Staab, Dave <i47@ornl.gov>
2025-01-03 16:22:27 -0500

    update time summary

--- src/components/summary/TimeSummary.vue
+++ src/components/summary/TimeSummary.vue
@@ -1,29 +1,18 @@
-<script>
+<script lang="ts" setup>
+import type { EntryPoint } from '@/utils';
 import ByCategory from './ByCategory.vue';
 import TkEntries from './TkEntries.vue';
 
-export default {
-  components: {
-    ByCategory,
-    TkEntries,
-  },
-  props: {
-    times: {
-      type: Object,
-      required: true,
-    },
-    data: {
-      type: Array,
-      required: true,
-    },
-  },
-};
+const props = defineProps<{
+  times: Record<string, number>;
+  data: EntryPoint[];
+}>();
 </script>
 
 <template>
   <div>
     <div class="text-3xl my-4">Time Summary</div>
-    <by-category :times="times" />
-    <tk-entries :entries="data" />
+    <by-category :times="props.times" />
+    <tk-entries :entries="props.data" />
   </div>
 </template>
